c90010137552093d1cfd02bc96f0a89db2102f1d
congpx-udu <congpx.ptit@gmail.com>
2025-09-16 13:45:38 +0700

    fix bài 1

--- Main.java
+++ Main.java
@@ -1,23 +1,53 @@
-import java.util.Scanner;
-
 public class Main {
+    private int x;
+    private int y;
 
-    public static int ChuVi(int x, int y) {
-        return 2 * (x + y);
+    public Main(int x, int y){
+        if (x > 0) {
+            this.x = x;
+        }
+        if (y > 0) {
+            this.y = y;
+        }
     }
 
-    public static int DienTich(int x, int y) {
+    public void setWidth(int x) {
+        if (x > 0) {
+            this.x = x;
+        }
+    }
+
+    public void setHeight(int y) {
+        if (y > 0) {
+            this.y = y;
+        }
+    }
+
+    public int getWidth() {
+        return x;
+    }
+
+    public int getHeight() {
+        return y;
+    }
+
+    public int dienTich() {
         return x * y;
     }
 
+    public int chuVi() {
+        return 2 * (x + y);
+    }
+
+    public void display() {
+        System.out.println(chuVi());
+        System.out.println(dienTich());
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        if (x > 0 && y > 0) {
-            System.out.println(ChuVi(x, y));
-            System.out.println(DienTich(x, y));
-        }
-        sc.close();
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        Main hcn = new Main(x, y);
+        hcn.display();
     }
 }
